d32f0db61b6db075e78dabc104ee92e6048a97b6
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-25 08:51:49 +0700

    initial commit

--- app/src/main/java/com/example/repro/api/ApiService.java
+++ app/src/main/java/com/example/repro/api/ApiService.java
@@ -18,9 +18,9 @@ public interface ApiService {
     @POST("post_pemasok.php")
     Call<ResponseBody> registerUser(@Body RequestBody body);
 
-    @GET("get_ambil.php")
-    Call<List<Ambil>> getAmbil();
-
     @GET("get_pemasok.php")
     Call<ApiResponse<List<Pemasok>>> getPemasokList();
+
+    @GET("get_ambil.php")
+    Call<List<Ambil>> getAmbil();
 }
