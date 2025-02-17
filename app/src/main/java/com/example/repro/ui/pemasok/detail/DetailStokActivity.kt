0cac4daf02f60010f081f6831bd780efad33e339
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-02-17 15:21:25 +0700

    initial commit

---
+++ app/src/main/java/com/example/repro/ui/pemasok/detail/DetailStokActivity.kt
@@ -0,0 +1,17 @@
+package com.example.repro.ui.pemasok.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.repro.R
+
+class DetailStokActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_stok)
+
+    }
+}
